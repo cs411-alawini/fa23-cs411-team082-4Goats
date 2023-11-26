52753aa5eeb6f05a9b394aabc584986e4ee7c25e
Keshav Trikha <ktrikha2@illinois.edu>
2023-11-26 10:42:10 -0600

    adding sql function

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,23 @@
 from flask import Flask
+import mysql.connector
 
 api = Flask(__name__)
+
+def sqlquery(query):
+
+    mydb = mysql.connector.connect(
+        host="104.198.145.57",
+        user="root",
+        password="team82",
+        database="US_youtube"
+    )
+
+    mycursor = mydb.cursor()
+    mycursor.execute(query)
+    myresult = mycursor.fetchall()
+
+    return myresult
+
 
 @api.route('/test')
 def my_profile():
